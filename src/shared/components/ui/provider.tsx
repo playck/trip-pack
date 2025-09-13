c1049766c0a5abe8d3214cc0dad0b0c7386a7ef6
playck <playck@naver.com>
2025-09-13 13:55:17 +0900

    feat: Toaster 컴포넌트 Provider에 추가

--- src/shared/components/ui/provider.tsx
+++ src/shared/components/ui/provider.tsx
@@ -3,6 +3,7 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+import { Toaster } from "./toaster";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +19,7 @@ export function Provider(props: ColorModeProviderProps) {
     <QueryClientProvider client={queryClient}>
       <ChakraProvider value={defaultSystem}>
         <ColorModeProvider {...props} />
+        <Toaster />
       </ChakraProvider>
     </QueryClientProvider>
   );
