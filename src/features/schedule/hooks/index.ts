3ad08252e17b2fed35d882b30168208fb87b6e75
playck <playck@naver.com>
2025-10-18 01:14:03 +0900

    [Schedule] feat: 일정 삭제 API 및 훅 추가

--- src/features/schedule/hooks/index.ts
+++ src/features/schedule/hooks/index.ts
@@ -2,5 +2,7 @@ export { useGeocoding } from "./useGeocoding";
 export { usePlacesAutocomplete } from "./usePlacesAutocomplete";
 export type { PlaceResult } from "./usePlacesAutocomplete";
 export { useCreateSchedule } from "./useCreateSchedule";
+export { useUpdateSchedule } from "./useUpdateSchedule";
+export { useDeleteSchedule } from "./useDeleteSchedule";
 export { useTripSchedules } from "./useTripSchedules";
 export { useSchedulesByDay } from "./useSchedulesByDay";
