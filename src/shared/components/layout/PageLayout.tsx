7fed470f3d99fc4315537e1126079e567807409d
playck <playck@naver.com>
2025-11-19 23:32:02 +0900

    style: PageLayout 컴포넌트에 style prop 추가

--- src/shared/components/layout/PageLayout.tsx
+++ src/shared/components/layout/PageLayout.tsx
@@ -3,11 +3,12 @@ import type { ReactNode } from "react";
 
 interface PageLayoutProps {
   children: ReactNode;
+  style?: React.CSSProperties;
 }
 
-export default function PageLayout({ children }: PageLayoutProps) {
+export default function PageLayout({ children, style }: PageLayoutProps) {
   return (
-    <Box px={3} w="full">
+    <Box px={3} w="full" style={style}>
       {children}
     </Box>
   );
