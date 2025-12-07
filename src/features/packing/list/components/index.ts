991f69ddc30d55c11f57c3f2db6b903b3ce80e4a
playck <playck@naver.com>
2025-12-07 16:49:22 +0900

    [Packing] refactor: TripTitle 컴포넌트 삭제

--- src/features/packing/list/components/index.ts
+++ src/features/packing/list/components/index.ts
@@ -4,6 +4,5 @@ export { default as GridView } from "./GridView";
 export { default as ListView } from "./ListView";
 export { default as ProgressBar } from "./ProgressBar";
 export { default as CheckListCopySheet } from "./CheckListCopySheet";
-export { default as TripTitle } from "./TripTitle"; // 추후 삭제 예정이나 호환성 유지
 export { default as ViewModeToggle } from "./ViewModeToggle";
 export * from "./types";
