c98be399006a6e035dca24e957b6f4efdfb3a25e
playck <playck@naver.com>
2025-12-09 00:13:44 +0900

    feat: tanstack router - pendingComponent 설정 추가

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 
 import Header from "../shared/components/layout/Header";
+import LoadingSpinner from "../shared/components/LoadingSpinner";
 
 export const Route = createRootRoute({
   component: RootComponent,
+  pendingComponent: () => <LoadingSpinner fullScreen centered />,
 });
 
 function RootComponent() {
