811886c4dbe59e254b1fbf2494ff25a47fd9b9b3
playck <playck@naver.com>
2025-12-29 21:34:25 +0900

    [Schedule] feat: MapWrapper 컴포넌트 추가

--- src/features/schedule/components/index.ts
+++ src/features/schedule/components/index.ts
@@ -1,16 +1,13 @@
 export { default as GoogleMapView } from "./GoogleMapView";
 export { default as Marker } from "./Marker";
-export { default as RouteLine } from "./RouteLine";
-export { default as DayScheduleCard } from "./DayScheduleCard";
-export { default as DayScheduleList } from "./DayScheduleList";
 export { default as AddScheduleSheet } from "./AddScheduleSheet";
-export { default as AddMemoSheet } from "./AddMemoSheet";
-export { default as ScheduleItem } from "./ScheduleItem";
-export { default as SwipeableScheduleItem } from "./SwipeableScheduleItem";
-export { default as DeleteScheduleModal } from "./DeleteScheduleModal";
-export { default as TripHeader } from "./TripHeader";
+export { default as DayScheduleList } from "./DayScheduleList";
+export { default as DayScheduleCard } from "./DayScheduleCard";
 export { default as EditableScheduleList } from "./EditableScheduleList";
 export { default as ScheduleActionButtons } from "./ScheduleActionButtons";
+export { default as DeleteScheduleModal } from "./DeleteScheduleModal";
 export { default as MoveDateBottomSheet } from "./MoveDateBottomSheet";
-
-export * from "./schedule-items";
+export { default as AddMemoSheet } from "./AddMemoSheet";
+export { default as TripHeader } from "./TripHeader";
+export { default as MapWrapper } from "./MapWrapper";
+export { default as SwipeableScheduleItem } from "./SwipeableScheduleItem";
