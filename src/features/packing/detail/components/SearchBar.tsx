0e0a9ff0d1ef114ae5f2a1d217953c1e1ce34da9
playck <playck@naver.com>
2026-01-31 17:09:41 +0900

    [CheckList] feat: SearchBar 공통 컴포넌트 수정 - BoxProps 속성 추가

--- src/features/packing/detail/components/SearchBar.tsx
+++ src/features/packing/detail/components/SearchBar.tsx
@@ -1,11 +1,11 @@
-import { Box } from "@chakra-ui/react";
+import { Box, type BoxProps } from "@chakra-ui/react";
 import ItemSearch from "./ItemSearch";
 
-interface SearchBarProps {
+interface SearchBarProps extends BoxProps {
   onSearch: (query: string) => void;
 }
 
-export function SearchBar({ onSearch }: SearchBarProps) {
+export function SearchBar({ onSearch, ...boxProps }: SearchBarProps) {
   return (
     <Box
       bg="white"
@@ -15,6 +15,7 @@ export function SearchBar({ onSearch }: SearchBarProps) {
       position="sticky"
       top="96px"
       zIndex={99}
+      {...boxProps}
     >
       <ItemSearch onSearch={onSearch} />
     </Box>
