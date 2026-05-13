347b398c4d8ef7a9f57f14dfa244715cda8aa6fd
playck <playck@naver.com>
2026-05-13 17:23:05 +0900

    [Packing] fix: ImportTextSheet 컴포넌트 높이 속성 수정

--- src/features/packing/list/components/ImportTextSheet.tsx
+++ src/features/packing/list/components/ImportTextSheet.tsx
@@ -176,7 +176,7 @@ export default function ImportTextSheet({
         onClose={handleClose}
         onBack={handleBack}
         title={`${sectionLabel} 추가 - 변환 결과`}
-        maxHeight="85vh"
+        expanded
         primaryButton={{
           text: "이 목록으로 추가하기",
           onClick: handleConfirm,
@@ -268,8 +268,7 @@ export default function ImportTextSheet({
       isOpen={isOpen}
       onClose={handleClose}
       title={`${sectionLabel} 추가`}
-      minHeight="80vh"
-      maxHeight="90vh"
+      expanded
       adjustForKeyboard={true}
       closeOnInteractOutside={!showExamples}
       primaryButton={{
